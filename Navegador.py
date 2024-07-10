31bcc2e87550f59adb41b2e3a936b0e972f38109
Leonardo <leomarino1999@gmail.com>
2024-07-10 19:08:50 -0300

    Modificacao Navegador.py e remocao de pseudocodigo.py

--- Navegador.py
+++ Navegador.py
@@ -4,7 +4,7 @@ from Importacao import Tarefas
 from Produto import Produto 
 
 # comando "pyautogui.PAUSE" = tempo de execução/intervalo de um comando para o outro
-pyautogui.PAUSE = 3 
+pyautogui.PAUSE = 2 
 
 # Criando a classe "Navegador"
 class Navegador:
@@ -22,7 +22,7 @@ class Navegador:
         pyautogui.write('https://dlp.hashtagtreinamentos.com/python/intensivao/login')
         pyautogui.press('enter') 
 # comando "time.sleep(x=tempo)" = tempo de carregamento do comando executado
-        time.sleep(5) 
+        time.sleep(3) 
 
 # metodo 3 = fazer login
     def fazer_login(self, email, senha):
@@ -31,12 +31,13 @@ class Navegador:
         pyautogui.press('tab')
         pyautogui.write(senha)
         pyautogui.press('enter')
-        time.sleep(5) 
+        time.sleep(3) 
 
 # metodo 4 = cadastro produto
     def cadastro_produtos(self, lista_produtos): 
 
         for produto in lista_produtos: 
+            pyautogui.press('tab') 
             pyautogui.write(produto.codigo)
             pyautogui.press('tab')
             pyautogui.write(produto.marca)
@@ -50,8 +51,14 @@ class Navegador:
             pyautogui.write(produto.custo)
             pyautogui.press('tab')
             pyautogui.write(produto.obs) 
-
-            pyautogui.press('enter') 
-            time.sleep(5) 
+            pyautogui.press('enter')
+            pyautogui.press('tab')
+            pyautogui.press('tab')
+            pyautogui.press('tab')
+            pyautogui.press('tab')
+            pyautogui.press('tab')
+            pyautogui.press('tab')
+            pyautogui.press('tab') 
+            time.sleep(3) 
           
             pyautogui.scroll(5000) 
